3a63968bf986394d1904bfcfab627fccda073d95
sathwikv2005 <sathwikv2005@gmail.com>
2026-03-31 19:50:28 +0530

    use multiple envs for training

--- env.py
+++ env.py
@@ -1,13 +1,15 @@
 import gymnasium as gym
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecFrameStack
 from stable_baselines3.common.monitor import Monitor
 
-def make_env(render_mode=None):
+def make_env(rank, render_mode=None):
     def _init():
         env = gym.make("CarRacing-v2", render_mode=render_mode)
         env = Monitor(env)
         return env
     return _init
 
-def get_env(render_mode=None):
-    return DummyVecEnv([make_env(render_mode)])
+def get_env(n_envs=4, render_mode=None):
+    env = SubprocVecEnv([make_env(i, render_mode) for i in range(n_envs)])
+    env = VecFrameStack(env, n_stack=4)
+    return env
