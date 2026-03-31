f1afbec3c2d015b8d87a0a0982f6516e2ff794e7
sathwikv2005 <sathwikv2005@gmail.com>
2026-03-31 20:53:10 +0530

    improve training speed by using cuda drivers

--- train.py
+++ train.py
@@ -1,15 +1,31 @@
 from stable_baselines3 import PPO
 from env import get_env
+import torch
 
-env = get_env()
+def main():
+    print("GPU:", torch.cuda.is_available())
 
-model = PPO(
-    "CnnPolicy",
-    env,
-    verbose=1,
-    tensorboard_log="./logs/"
-)
+    env = get_env(n_envs=4)
 
-model.learn(total_timesteps=100_000)
+    model = PPO(
+        "CnnPolicy",
+        env,
+        verbose=1,
+        tensorboard_log="./logs/",
+        device="cuda",
 
-model.save("model/ppo_carracing")
+        n_steps=1024,
+        batch_size=256,
+        learning_rate=3e-4,
+        gamma=0.99,
+        gae_lambda=0.95,
+        clip_range=0.2,
+        ent_coef=0.01,
+    )
+
+    model.learn(total_timesteps=500_000)
+    model.save("model/ppo_carracing")
+
+
+if __name__ == "__main__":
+    main()
