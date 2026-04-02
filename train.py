c552b6b4ff9bcf2902fffdc17ccf7080e39ccabb
mohanv2005 <dantulurimohan10@gmail.com>
2026-04-02 16:52:12 +0530

    update learning rate and ent_coef

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CheckpointCallback
 from env import get_env
 import torch
+
 
 def main():
     print("GPU:", torch.cuda.is_available())
@@ -16,14 +18,30 @@ def main():
 
         n_steps=512,
         batch_size=256,
-        learning_rate=3e-4,
+        n_epochs=10,
+
+        learning_rate=2e-4,      
+        ent_coef=0.003,          
+
         gamma=0.99,
         gae_lambda=0.95,
         clip_range=0.2,
-        ent_coef=0.005,
+
+        vf_coef=0.5,
+        max_grad_norm=0.5,
     )
 
-    model.learn(total_timesteps=1_000_000)
+    checkpoint_callback = CheckpointCallback(
+        save_freq=100_000,
+        save_path="./models/",
+        name_prefix="ppo_carracing"
+    )
+
+    model.learn(
+        total_timesteps=1_000_000,
+        callback=checkpoint_callback
+    )
+
     model.save("model/ppo_carracing_custom_reward")
 
 
