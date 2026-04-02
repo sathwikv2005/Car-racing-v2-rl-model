6032f7997564986d119fb4493739c3fe50aa9b76
mohanv2005 <dantulurimohan10@gmail.com>
2026-04-02 16:48:52 +0530

    add previous action for custom reward

--- env.py
+++ env.py
@@ -8,17 +8,33 @@ from utils import custom_reward
 class RewardWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
+        self.prev_action = np.array([0.0, 0.0, 0.0])
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        self.prev_action = np.array([0.0, 0.0, 0.0])
+        return obs, info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        shaped_reward = custom_reward(obs, reward, action)
+
+        # Apply custom reward with previous action
+        shaped_reward = custom_reward(obs, reward, action, self.prev_action)
+
+        # Update previous action
+        self.prev_action = action
+
         return obs, shaped_reward, terminated, truncated, info
 
 
 def make_env(rank, render_mode=None):
     def _init():
         env = gym.make("CarRacing-v2", render_mode=render_mode)
+
+        # Apply reward wrapper
         env = RewardWrapper(env)
+
+        # Monitor for logging
         env = Monitor(env)
 
         return env
@@ -28,5 +44,8 @@ def make_env(rank, render_mode=None):
 
 def get_env(n_envs=4, render_mode=None):
     env = SubprocVecEnv([make_env(i, render_mode) for i in range(n_envs)])
+
+    # Frame stacking
     env = VecFrameStack(env, n_stack=4)
+
     return env
